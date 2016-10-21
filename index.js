a800204c7690651e146c6906590f511d04f9d421
nadunindunil <nadun1indunil@gmail.com>
2016-10-22 04:15:13 +0530

    adding to array implemented

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 var utils = require('./lib/utils');
 
-// Example
 
+// utils.getTableSchema('interns','us-east-1').then(function(data){
+// 	console.log("\nHere Comes the Table \n");
+//     console.log(data);
+//
+// },function(err){
+//     console.log(err);
+// });
 
-utils.getTableSchema('Customer','eu-west-1').then(function(data){
-	console.log("\nHere Comes the Table \n")
-    console.log(data)
+var array = ["interns","task"];
+var region = "us-east-1";
 
-},function(err){
-    console.log(err)
-})
+var createTableArray = function(array,region) {
+    var tableArray = [];
+    var promises = [];
 
+    for (var i = 0; i < array.length ; i++) {
+        promises.push(utils.getTableSchema(array[i],region));
+    }
+
+    Promise.all(promises)
+      .then(function(results) {
+        console.log("All done", results);
+      })
+      .catch(function(e) {
+          console.log(e);
+      });
+
+};
+
+createTableArray(array,region);
